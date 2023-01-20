ab532ddb7d5aee9f6d403ca8b1fc9cffee1905eb
Alexey Li <alexeyli.sdet@gmail.com>
2023-01-19 18:25:22 -0800

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Alexey Li on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
