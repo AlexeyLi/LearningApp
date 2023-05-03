67730c641c574bd9872de531e19edc627a2c9992
alexeyli <alexeyli.sdet@gmail.com>
2023-05-03 11:13:31 -0700

    Add TestView and NavigationLink to it in HomeView

--- LearningApp/Views/ContentView.swift
+++ LearningApp/Views/ContentView.swift
@@ -32,7 +32,7 @@ struct ContentView: View {
                 
             }
             .padding()
-            .navigationTitle("Learn \(model.currentModule?.category ?? "")")
+            .navigationBarTitle("Learn \(model.currentModule?.category ?? "")")
             .accentColor(.black)
         }
     }
--- LearningApp/Views/HomeView.swift
+++ LearningApp/Views/HomeView.swift
@@ -30,7 +30,6 @@ struct HomeView: View {
                                         ContentView()
                                             .onAppear(perform: {
                                                 model.beginModule(module.id)
-                                                print(model.currentContentSelected)
                                             }),
                                     tag: module.id,
                                     selection: $model.currentContentSelected,
@@ -40,8 +39,16 @@ struct HomeView: View {
                                     HomeViewRow(image: module.content.image, title: "Learn \(module.category)", description: module.content.description, count: "\(module.content.lessons.count) Lessons", time: module.content.time)
                                 })
                                 
-                                // Test Card
-                                HomeViewRow(image: module.test.image, title: "\(module.category) Test", description: module.test.description, count: "\(module.test.questions.count) Lessons", time: module.test.time)
+                                NavigationLink(
+                                    destination:
+                                        TestView().onAppear(perform: {
+                                        model.beginTest(module.id)
+                                    }),
+                                    tag: module.id,
+                                    selection: $model.currentTestSelected) {
+                                        // Test Card
+                                        HomeViewRow(image: module.test.image, title: "\(module.category) Test", description: module.test.description, count: "\(module.test.questions.count) Lessons", time: module.test.time)
+                                    }
                             }
                         }
                     }
